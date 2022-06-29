49ee0cd7e8ef829f32e16057b8d40a525f4ba7db
ayushkul <ayushk@leewayhertz.com>
2022-06-29 12:46:47 +0530

    README.md updates

--- src/app/constants/index.rs
+++ src/app/constants/index.rs
@@ -1,3 +1,3 @@
-pub const DB_NAME: &str = "nftticketing-dev";
+pub const DB_NAME: &str = "nft-dev";
 pub const USER_COLLECTION: &str = "nt-users";
-pub const DB_URL: &str = "mongodb://c:sillyclam29@nftticketing-dev-shard-00-00.ztelk.mongodb.net:27017,nftticketing-dev-shard-00-01.ztelk.mongodb.net:27017,nftticketing-dev-shard-00-02.ztelk.mongodb.net:27017/nftticketing-dev?ssl=true&retryWrites=true&w=majority&replicaSet=atlas-t5kvmc-shard-0&authSource=admin";
+pub const DB_URL: &str = "DB_URL";
